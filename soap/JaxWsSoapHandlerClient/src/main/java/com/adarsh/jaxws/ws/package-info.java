84b6f412a98255410d9765a3bdf4b1b8e1670c0a
adarkuma <adarkuma@cisco.com>
2016-05-06 10:21:33 +0530

    ading

--- soap/JaxWsSoapHandlerClient/src/main/java/com/adarsh/jaxws/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.jaxws.adarsh.com/")
-package com.adarsh.jaxws.ws;
--- soap/SpringJaxwsWebServicesExample/src/main/java/com/adarsh/spring/sws/beans/HelloWorldBo.java
+++
@@ -1,7 +0,0 @@
-package com.adarsh.spring.sws.beans;
-
-public interface HelloWorldBo{
-
-	public String getHelloWorld();
-	
-}
